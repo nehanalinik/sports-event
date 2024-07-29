87e89f54a27e303a18b2afdf1ae8456fb15f0e72
neha-kennedy <nehanalinik@gmail.com>
2024-07-29 13:11:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ function App() {
     <section>
       <div className="background"></div>
       <div className="overflow-auto h-[99vh] bg-white/10 z-10 m-1 rounded-md scroll-smooth">
-        <div className="flex flex-col p-4 items-center justify-center">
-          <div className="flex items-center">
+        <div className="flex flex-col p-4 items-center justify-center w-full">
+          <div className="flex items-center w-full">
           <img src={Logo} alt="logo" width={110} />
-          <div className=" w-[100%] flex flex-col">
-              <div className="text-md xsm:text-lg sm:text-xl smd:text-2xl md:text-3xl lg:text-4xl text-slate-200">
+          <div className=" w-[80%] flex flex-col items-center">
+              <div className="text-xl md:text-2xl lg:text-4xl text-slate-200">
               BPBO Sports Event 
               </div>
-              <div className="text-sm xsm:text-md sm:text-lg smd:text-xl md:text-2xl font-bold text-slate-200 tracking-wide">
+              <div className="text-lg md:text-xl lg:text-2xl font-bold text-slate-200 tracking-wide">
                2024-25
               </div>
             </div>
